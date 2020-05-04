3dc2ea48bb4e8e0a2c41ae94f55dc7a9db75a4f1
Vitalii Didyk <axelzero31@gmail.com>
2020-05-04 20:25:32 +0300

    Change screen mode, added UI

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] private AudioClip _brakeClip;
+                     private LevelManager level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<LevelManager>();
+        level.CountUpOfBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
+        BlockDestroy();
+    }
+
+    private void BlockDestroy()
+    {
+        level.AddScore();
         AudioSource.PlayClipAtPoint(_brakeClip, transform.position);
-        Destroy(gameObject, 0.05f);
+        level.BlockDesroyed();
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private int brekeBlocks = 0;
+                     private SceneLoader sceneLoader;
+
+    [SerializeField, Range(0.1f, 2f)] private float _gameSpeed = 1f;
+
+    [SerializeField] private int currentScore = 0;
+    [SerializeField] private int pointsDestroy = 10;
+    [SerializeField] private TextMeshProUGUI _txtScore;
+
+
+
+
+    private void Awake()
+    {
+        int levelCount = FindObjectsOfType<LevelManager>().Length;
+        if (levelCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+        _txtScore.text = currentScore.ToString();
+    }
+    private void Update()
+    {
+        // Time.timeScale = _gameSpeed;
+    }
+    public void AddScore()
+    {
+        currentScore += pointsDestroy;
+        _txtScore.text = currentScore.ToString();
+    }
+    public void CountUpOfBlocks() 
+    {
+        brekeBlocks++;
+    }
+    public void BlockDesroyed() 
+    {
+        brekeBlocks--;
+        LoadNextLevel();
+    }
+    private void LoadNextLevel() 
+    {
+        if (brekeBlocks <= 0) 
+        {
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
